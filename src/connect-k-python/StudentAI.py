ba3159c48189e3c93ec7f2ff93f606e0dda098f7
Xiao Zhang <aiden@dhcp-v079-120.mobile.uci.edu>
2019-01-25 15:18:27 -0800

    Initializing, copying all files#

---
+++ src/connect-k-python/StudentAI.py
@@ -0,0 +1,24 @@
+from random import randint
+from BoardClasses import Move
+from BoardClasses import Board
+#The following part should be completed by students.
+#Students can modify anything except the class name and exisiting functions and varibles.
+class StudentAI():
+    col = 0
+    row = 0
+    k = 0
+    g = 0
+
+    def __init__(self,col,row,k,g):
+        self.g = g
+        self.col = col
+        self.row = row
+        self.k = k
+        self.board = Board(col,row,k,g)
+
+    def get_move(self,move):
+        self.board.make_move(move,1)
+        if self.g == 0:
+            return Move(randint(0,self.col-1),randint(0,self.row-1))
+        else:
+            return Move(randint(0,self.col-1),0)
