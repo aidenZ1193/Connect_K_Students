72db3eedcbf72c5856ebc87a56f341e5f349cc5a
Xiao Zhang <aiden@dhcp-v024-028.mobile.uci.edu>
2019-02-01 16:51:30 -0800

    Adding certain functions Untedted

--- src/connect-k-cpp/StudentAI.cpp
+++ src/connect-k-cpp/StudentAI.cpp
@@ -6,7 +6,11 @@
 StudentAI::StudentAI(int col,int row,int k,int g)
 	:AI(col, row, k, g)
 {
-	
+	my_board = Board(col, row, k, g);
+	min_col = col/2-2;
+	max_col = col/2+2;
+	min_row = row/2-2;
+	max_row = row/2+2;
 }
 
 Move StudentAI::GetMove(Move board)
@@ -17,3 +21,39 @@ Move StudentAI::GetMove(Move board)
 		return Move(rand() % (col - 1), 0);
 }
 
+// @return: board's evaluation score
+// @usage: emulate AI's move and the following moves
+int StudentAI::max_move(pair<int, int> &spot, int depth){
+
+}
+
+// @return: evaluation score of the line
+// @usage：based on the line'e empty spots and number of same color pieces
+int StudentAI::score_dict(int empty, int pieces, int k){
+	if(pieces >= k) return pow(10, k);
+	k--;
+	while(k > 0){
+		if(pieces == k){
+			if(empty >= 2) return pow(10, k);
+			else if(empty == 1) return pow(10, k-1);
+		} 
+		k--;
+	}
+	return 0;
+}
+
+// @usage: find all valid moves in the board (within constrains)
+void StudentAI::find_empty(vector<pair<int, int> > &valid){
+	for(int i = min_row; i < max_row; i++){
+		for(int j = min_col; j < max_col; j++ ){
+			if(my_board.board[i][j] == 0) valid.push_back(make_pair(i, j));
+		}
+	}
+}
+
+
+
+// @return: score for the whole board
+int StudentAI::evaluate_board(){
+
+}
--- src/connect-k-cpp/StudentAI.h
+++ src/connect-k-cpp/StudentAI.h
@@ -1,6 +1,8 @@
 #ifndef STUDENTAI_H
 #define STUDENTAI_H
 #include "AI.h"
+#include <math.h> 
+#include <unordered_map>
 #pragma once
 
 //The following part should be completed by students.
@@ -10,6 +12,43 @@ class StudentAI :public AI
 public:
 	StudentAI(int col, int row, int k, int g);
 	virtual Move GetMove(Move board);
+
+	//vector<pair<int, int> > valid;
+	// keep track of current range of pieces on the board
+	// when perform the search, use these +/-2 to lower valid spots
+	int min_col, max_col, min_row, max_row;
+
+	// stores the current board with 0 = empty, 1&2 for black&white
+	Board my_board;
+	// stores the evaluation score for each empty spot, for AI and the other player(rival)
+	unordered_map<pair<int, int>, int> AI_values;
+	unordered_map<pair<int, int>, int> rival_values;
+
+
+	// @return: board's evaluation score
+	// @usage: emulate AI's move and the following moves
+	int max_move(pair<int, int> &spot, int depth);
+
+	// @return: board's evalutation score
+	// @usage: emulate component's move and the following moves
+	int min_move(pair<int, int> &spot, int depth);
+
+	// @return: evaluation score of the line
+	// @usage：based on the line'e empty spots and number of same color pieces
+	int score_dict(int empty, int pieces, int k);
+
+	// @return evaluation score for the empty spot valid[i][j]
+	// @usage: evaluate all eight directions for the space
+	int evaluate_single_space(int i, int j, int turn);
+
+	// @return: score for the whole board
+	int evaluate_board();
+
+	// @usage: find all valid moves in the board (within constrains)
+	void find_empty(vector<pair<int, int> > &valid);
+
+
+
 };
 
 #endif //STUDENTAI_H
