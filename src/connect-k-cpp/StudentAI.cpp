0fa802751b6fc31c576529f83a5c77a2181163ff
Xiao Zhang <aiden@vcv065100.vpn.uci.edu>
2019-02-22 23:45:03 -0800

    Trying to modify the eveluate function

--- src/connect-k-cpp/StudentAI.cpp
+++ src/connect-k-cpp/StudentAI.cpp
@@ -64,7 +64,7 @@ Move StudentAI::GetMove(Move board)
 	find_empty(valid);
 	int len = valid.size();
 	int max_score = INT_MIN;
-	int depth = 1;	// max - min - max- min - max
+	int depth = 2;	// max - min - max- min - max
 	for(int i = 0; i < len; i++){
 		// change color in the spot to 1, which is the AI's move
 		my_board.board[valid[i].first][valid[i].second] = 1;
