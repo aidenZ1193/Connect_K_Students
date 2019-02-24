0d9e76e44a102f5bf8d96312ea1e38a8fd86f201
Xiao Zhang <aiden@vcv065111.vpn.uci.edu>
2019-02-24 15:22:31 -0800

    Try with poor ai

--- src/connect-k-cpp/StudentAI.cpp
+++ src/connect-k-cpp/StudentAI.cpp
@@ -97,9 +97,9 @@ Move StudentAI::GetMove(Move board)
 	m.col = usable[i].second;
 
 	// debug
-	cout<<"the list of usable in get_move:"<<endl;
-	for(int i = 0; i < usable.size(); i++)
-		cout<<"i = "<<i<<" usable is "<<usable[i].first<<" "<<usable[i].second<<endl;
+	//cout<<"the list of usable in get_move:"<<endl;
+	//for(int i = 0; i < usable.size(); i++)
+	//	cout<<"i = "<<i<<" usable is "<<usable[i].first<<" "<<usable[i].second<<endl;
 
 
 	my_board.board[m.row][m.col] = 1;
@@ -380,7 +380,7 @@ int StudentAI::evaluate_both(int i, int j){
 	ai = evaluate_board(1);
 	opponent = evaluate_board(2);
 
-	cout<<"i = "<<i<<", j = "<<j<<" ai score = "<<ai<<" opponent score = "<<opponent<<endl;
+	//cout<<"i = "<<i<<", j = "<<j<<" ai score = "<<ai<<" opponent score = "<<opponent<<endl;
 	return ai + opponent;
 	// before furture adjusting: this version beats poorAI in no gravity modes mostly.
 }
