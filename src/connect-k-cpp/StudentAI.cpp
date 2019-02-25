5ad2a2cb3a6a2b496c1aaf4757bd41d786405cf6
Xiao Zhang <aiden@vcv067040.vpn.uci.edu>
2019-02-24 21:24:26 -0800

    See about depth = 1

--- src/connect-k-cpp/StudentAI.cpp
+++ src/connect-k-cpp/StudentAI.cpp
@@ -73,7 +73,7 @@ Move StudentAI::GetMove(Move board)
 	find_empty(valid);
 	int len = valid.size();
 	int max_score = INT_MIN;
-	int depth = 3;	// max - min - max- min - max
+	int depth = 1;	// max - min - max- min - max
 
 	// for alpha-beta pruning
 	int alpha = INT_MIN, beta = INT_MAX;
