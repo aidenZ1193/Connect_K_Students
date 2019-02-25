730b175f8caf3510017b8a9d1eb9d6564651afb4
Xiao Zhang <aiden@vcv067040.vpn.uci.edu>
2019-02-24 21:33:19 -0800

    k+5 too big??

--- src/connect-k-cpp/StudentAI.cpp
+++ src/connect-k-cpp/StudentAI.cpp
@@ -209,7 +209,7 @@ int StudentAI::max_move(pair<int, int> &spot, int depth, int &alpha, int &beta){
 // @return: evaluation score of the line
 // @usage：based on the line'e empty spots and number of same color pieces
 int StudentAI::score_dict(int empty, int pieces, int k){
-	if(pieces >= k-1) return pow(10, k+5);
+	if(pieces >= k-1) return pow(10, k+4);
 	//if(pieces >= k-2) return pow(10, k+3);
 	if(pieces == 0) return 0;
 	k--;
